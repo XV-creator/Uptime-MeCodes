bc5d5cd9e9ff990c2c0d79d3ca7dbaa0aa0d266e
xSimplez <69173408+xSimplez@users.noreply.github.com>
2020-08-24 02:19:37 +0200

    Update botekle.js

--- komutlar/botekle.js
+++ komutlar/botekle.js
@@ -1,15 +1,15 @@
 module.exports={
-	name:'ume',
-	aliases:[],
+	name:'uptime',
+	aliases:["monitor"],
 	cooldown:0,
 	execute(message,args){
     const Discord=require("discord.js");
 		const db=require("quick.db")
-    if(!args[0])return message.channel.send(new Discord.MessageEmbed().setAuthor('تحذير').setDescription(":slight_smile: أدخل اسم المشروع").setColor('RANDOM'));
-    if(args[0].startsWith("http://")||args[0].startsWith("https://")||args[0].includes(".")||args[0].includes("/"))return message.reply("لا يمكنك إضافة Url ضيف اسم المشروع فقط :upside_down:");
-    if(db.get("uptime").includes(args[0]+" "+message.author.id))return message.reply("هذا المشروع مرفق بالفعل :heartpulse:")
+    if(!args[0])return message.channel.send(new Discord.MessageEmbed().setColor('#00BFFF').setDescription("Please give website link to moniter"));
+    if(args[0].startsWith("http://")||args[0].startsWith("https://")||args[0].includes(".")||args[0].includes("/"))return message.channel.send(new Discord.MessageEmbed().setDescription("You must type the project name").addField("Example:",`!monitor MeCodes.glitch.me`));
+    if(db.get("uptime").includes(args[0]+" "+message.author.id))return message.channel.send(new Discord.MessageEmbed().setDescription("This Project already uptimed"))
     db.push("uptime",args[0]+" "+message.author.id);
-    message.reply(args[0]+"  برجاء الانتظار 30 ثانية و مشروعك هيعمل بنجاح :robot:!!");
-    message.client.channels.cache.get("741202677295349841").send(`${message.client.guilds.cache.get("677938232142331935").member(message.author)||message.author.tag} `+args[0]+" تمت إضافة المشروع!!.");
+    message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDescription("Just Wait `30 Seconds` For The Uptime Your Project"))
+    message.client.channels.cache.get("747245657190039583").send(`${message.client.guilds.cache.get("589156112448749589").member(message.author)||message.author.tag} `+args[0]+" تمت إضافة المشروع!!.");
 	}
 };
