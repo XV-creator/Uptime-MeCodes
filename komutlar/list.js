a2757eb4ec3747c3001b2aa15c863885e8fdc7f7
xSimplez <69173408+xSimplez@users.noreply.github.com>
2020-08-24 02:30:28 +0200

    Update list.js

--- komutlar/list.js
+++ komutlar/list.js
@@ -5,6 +5,6 @@ module.exports={
     const Discord=require("discord.js");
     const db=require("quick.db");
     var projects=db.fetch("uptime").filter(a=>a.split(" ")[1]==message.author.id).map(a=>a.split(" ")[0]).join("\n");
-		message.channel.send(new Discord.MessageEmbed().setTitle('هذه هي مشاريعك').setAuthor(message.author.username,message.author.displayAvatarURL({dynamic:true})).setDescription(projects==""?"Projen yok":projects).setColor('RANDOM'))
+message.channel.send(new Discord.MessageEmbed().setTitle('Your Projects').setAuthor(message.author.username,message.author.displayAvatarURL({dynamic:true})).setDescription(projects==""?"Error 404":projects).setColor('RANDOM'))
 	}
 };
