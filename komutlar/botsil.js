2e0ed955d78c475d44bf1b662193d2cc88692d64
xSimplez <69173408+xSimplez@users.noreply.github.com>
2020-08-24 02:27:38 +0200

    Update botsil.js

--- komutlar/botsil.js
+++ komutlar/botsil.js
@@ -5,9 +5,10 @@ module.exports={
 	execute(message,args){
     const Discord=require("discord.js");
 		const db=require("quick.db")
-      if(!args[0])return message.channel.send(new Discord.MessageEmbed().setAuthor('تحذير').setDescription("أدخل اسم المشروع :skull_crossbones:").setColor('RANDOM'));
-    if(!db.get("uptime").includes(args[0]+" "+message.author.id))return message.reply("هذا المشروع غير موجود :confused:")
+       if(!args[0])return message.channel.send(new Discord.MessageEmbed().setColor('#00BFFF').setDescription("Please give website link to delete"));
+   if(!args[0])return message.channel.send(new Discord.MessageEmbed().setDescription("You must type tye project name").setColor('#00BFFF'));
+    if(!db.get("uptime").includes(args[0]+" "+message.author.id))return message.channel.send(new Discord.MessageEmbed().setDescription("I Didnt Find This Project"))
     db.set("uptime",db.get("uptime").filter(a=>a!=args[0]+" "+message.author.id));
-    message.reply(args[0]+" :skull: تم حذف المشروع");
+    message.channel.send(new Discord.MessageEmbed("Your Project Has Been Deleted!"));
    }
 };
